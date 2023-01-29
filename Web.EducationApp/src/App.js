d288b30d6edbedaaee504bce243b5a65c57ec65b
vinish.maurya <vinish.maurya@binarysemantics.com>
2023-01-29 12:47:21 +0530

    Login Incomplete Screen

--- Web.EducationApp/src/App.js
+++ Web.EducationApp/src/App.js
@@ -7,24 +7,72 @@ import Todo from "./pages/Todo";
 import VinishTest from "./pages/VinishTest";
 import data from "./data/data";
 import BootstrapComponent from "./pages/BootstrapComponent";
+import Login from "./pages/login";
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 
 function App() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [user, setUser] = useState();
+
+    //// set the state of the user
+    //setUser(response.data);
+    //// store the user in localStorage
+    //localStorage.setItem('user', response.data);
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const user = { username, password };
+        // send the username and password to the server
+        const response = await axios.post(
+            "http://blogservice.herokuapp.com/api/login",
+            user
+        );
+        // set the state of the user
+        setUser(response.data)
+        // store the user in localStorage
+        localStorage.setItem('user', response.data)
+        console.log(response.data)
+    };
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem("user");
+        if (loggedInUser) {
+            const foundUser = JSON.parse(loggedInUser);
+            setUser(foundUser);
+        }
+    }, []);
+
+
+    // if there's a user show the message below
+    if (user) {
+        return (
+            <BrowserRouter>
+                
+                <AdminLayout>
+                    <Switch>
+                        {data.map((val, index) => (
+                            <Route path={val.route} key={index}>
+                                <BootstrapComponent data={val} />
+                            </Route>
+                        ))}
+                        <Route path="/vinish" component={VinishTest} />
+                        <Route path="/Todo" component={Todo} />
+                    </Switch>
+                </AdminLayout>
+            </BrowserRouter>
+        );
+    }
+
+    // if there's no user, show the login form
     return (
         <BrowserRouter>
-            <AdminLayout>
-                <Switch>
-
-                    {data.map((val, index) => (
-                        <Route path={val.route} key={index}>
-                            <BootstrapComponent data={val} />
-                        </Route>
-                    ))}
-                    <Route path="/vinish" component={VinishTest} />
-                    <Route path="/" component={Todo} />
-                </Switch>
-            </AdminLayout>
+            <Route path="/" component={Login} />
         </BrowserRouter>
     );
-}
+};
 
 export default App;
---
+++ Web.EducationApp/src/pages/login.js
@@ -0,0 +1,14 @@
+
+const pageTitle = "Login";
+const Login = () => {
+    return (
+        <>
+
+            <h2 className="page-title">{pageTitle}</h2>
+           
+        </>
+
+    );
+};
+
+export default Login;
